536dc0ac1f96ae97166de33b558868799b2ca127
Tanay Pant <tanaypant@protonmail.com>
2022-02-01 17:51:57 +0100

    Fix indentation

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import { OktaAuth } from '@okta/okta-auth-js'
 import OktaVue from '@okta/okta-vue'
 
 const oktaAuth = new OktaAuth({
-    issuer: 'https://{yourOktaDomain}/oauth2/default',
-    clientId: '{clientId}',
+  issuer: 'https://{yourOktaDomain}/oauth2/default',
+  clientId: '{clientId}',
   redirectUri: window.location.origin + '/login/callback',
   scopes: ['openid', 'profile', 'email']
 })
